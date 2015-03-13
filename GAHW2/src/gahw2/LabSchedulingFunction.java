d129ea83a5e7ce5047e0d51c3e62ac6d0e90d2ab
Cliff Rice <ce.rice@knights.ucf.edu>
2015-03-12 20:49:58 -0400

    Fixed small issue, added alternative fitness function (commented out)

--- GAHW2/src/gahw2/LabSchedulingFunction.java
+++ GAHW2/src/gahw2/LabSchedulingFunction.java
@@ -16,6 +16,7 @@ class LabSchedulingFunction extends FitnessFunction {
 			p3 = 25, 
 			p4 = 5, 
 			p_invalid = -3400, 
+			// p_valid = 1000,
 			shift_penalty = -700, // TODO: We need to discuss a value for this penalty
 			shifts_per_day = 5;
 
@@ -75,8 +76,69 @@ class LabSchedulingFunction extends FitnessFunction {
 			shift_discrepencies += Math.abs(shifts_per_day - numShifts[k]); // adds the absolute difference between actual and expected shift numbers
 		}
 		
-		int X.rawfitness += shift_discrepencies * shift_penalty; // Penalizes the score based on the number of shifts
+		X.rawfitness += shift_discrepencies * shift_penalty; // Penalizes the score based on the number of shifts
 	}
+  
+  
+  
+//  public void doRawFitness(Chromo X){
+//	  	
+//	  	// Start with 0 as the raw fitness
+//		X.rawFitness = 0;
+//		boolean valid = true;
+//
+//		// Initialize the number of shifts to 0 for each employee
+//		int[] numShifts = new int[8];
+//		
+//		for (int i = 0; i < numShifts.length; i ++){
+//			numShifts[i] = 0;
+//		}
+//		
+//		// Loop through the genes in the chromosome
+//		for (int z = 0; z < X.selection.length; z++){
+//			
+//			// Set the array-indexing variables
+//			int worker = X.selection[z]; // integer representation of gene from Chromo
+//			int day = z / shifts_per_day; // gets the day of the week
+//			int shift = z % shifts_per_day; // gets the shift of the day
+//			
+//			// Check the preference value, reward or penalize the score accordingly
+//			switch (preferences[worker][day][shift]) {
+//				case 0:
+//					valid = false;
+//					break;
+//				case 1:
+//					X.rawFitness += p1;
+//					break;
+//				case 2:
+//					X.rawFitness += p2;
+//					break;
+//				case 3:
+//					X.rawFitness += p3;
+//					break;
+//				case 4:
+//					X.rawFitness += p4;
+//					break;
+//				default:
+//					break;
+//			}
+//			
+//			// Track the shifts for this worker
+//			numShifts[worker]++;
+//		}
+//		
+//		if (numShifts[0] != 0) { valid = false; }; 
+//		
+//		for(int k = 1; k < numShifts.length; k++){
+//			if(numShifts[0] != shifts_per_day) { valid = false };
+//		}
+//		
+//		if (valid){
+//			X.rawfitness += p_valid;
+//		} else {
+//  		X.rawfitness = (X.rawfitness / (p1 * Parameters.numGenes)) * (p_valid + (Parameters.numGenes * p4) - 1);
+//		}
+//	}
 
 //PRINT OUT AN INDIVIDUAL GENE TO THE SUMMARY FILE *********************************
 
